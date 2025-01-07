2cf3428ea6126bb47cb54362718e1c72bb8961ca
dlanrll <beast143159@naver.com>
2025-01-08 01:08:11 +0900

    자습

--- code/python/13.py
+++ code/python/13.py
@@ -1,6 +1,6 @@
 # 리스트 내포
 # 파이썬에서 리스트를 간결하게 생성하는 문법
-'''
+
 numbers = [1, 2, 3, 4, 5]
 squares = [n**2 for n in numbers]
 print(squares)
@@ -13,36 +13,42 @@ even_squares = [x**2 for x in range(1, 6) if x % 2 == 0]
 print(even_squares)
 
 #구구단 실습
-numbers = [7]
-total = 0
-for num in numbers :
-    total += num
-    print("현재 total >> ", total)
-print(f"합계는 {total}입니다.")
+multiple = int(input("몇단을 출력할까요?: "))
 
-for i in range(1)
+for i in range(1, 10):  # 1부터 9까지 반복
+    print(f"{multiple} x {i} = {multiple * i}")
 
-fruits = {"apple": 95, "banana": 105, "cherry":50}
-rnrneks = input("몇단을 출력할까요? : ")
+# 실습, 정수 합 계산
+# 방법1
+n = int(input("어디까지 계산할까요? : "))
+odd_sum = sum(i for i in range(1, n + 1) if i % 2 != 0)
+print(f"1부터 {n}까지의 홀수의 합: {odd_sum}")
 
-for i in range : 
-    print(f"{fruit}의 칼로리는 {fruits[fruit]}Kcal 입니다.")
-else :
-    print("과일이 존재하지 않습니다.")
+# 방법2
+n = int(input("어디까지 계산할까요? : "))
+odd_sum = 0
+for i in range(1, n + 1):
+    if i % 2 != 0: 
+        odd_sum += i
+print(f"1부터 {n}까지의 홀수의 합: {odd_sum}")
 
-# 조건문과 함께 사용
+# 실습, 평균값 구하기
+students = {
+    "학생1" :{"국어":83, "영어":92, "수학":88},
+    "학생2" :{"국어":90, "영어":79, "수학":86},
+    "학생3" :{"국어":88, "영어":86, "수학":94}
+}
 
-#실습. 정수 합 계산
-#1부터 사용자가 입력한 수까지 홀수의 합 구하기
-while True:
-    user_input = input("어디까지 계산할까요?: ")
+subjects = ["국어", "영어", "수학"]
+averages = {}
 
-    number = int(user_input)
-    if number == (range(1, 6) if x % 2 =! 0) :
-        continue
+for subject in subjects:
+    total = sum(student[subject] for student in students.values())
+    averages[subject] = total / len(students)
 
-    total_sum = sum(number)
-    print(f"1부터 {number}까지의 합: {total_sum}")'''
+print("과목별 평균 점수:")
+for subject, avg in averages.items():
+    print(f"{subject}: {avg:.2f}점")
 
 #if와 elif의 차이는 if와 elif를 동시에 썼을 때 그 조건이 중첩일 경우 다음 코드로 내려가지 않음
 #결론 : 조건들이 겹치지 않는(상호베타적) 상황일 때는 if, elif, else를 작성해도 되지만
