90ee021e6499552e9e73edac9c47986a68aaf015
dlanrll <beast143159@naver.com>
2025-01-23 20:33:40 +0900

    seaborn

--- code/python/59.py
+++ code/python/59.py
@@ -1,16 +1,67 @@
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+'''
+penguins = sns.load_dataset("penguins")
 
-# 데이터셋 
-print(sns.get_dataset_names())
+# 1. 종(species)별 평균 몸무게(body_mass_g), bar
+plt.figure(figsize=(8, 6))
+sns.barplot(x="species", y="body_mass_g", data=penguins, palette="pastel")
+plt.show()
 
-# 예제데이터
-tips = sns.load_dataset("tips")
-# print(tips.info(0))
+# 2. 부리 길이(bill_length_mm)와 부리 깊이(bill_depth_mm)의 관계, scatterplot
+plt.figure(figsize=(8, 6))
+sns.scatterplot(
+    x="bill_length_mm", y="bill_depth_mm", hue="species", data=penguins, palette="pastel"
+)
+plt.show()
 
-sns.scatterplot(x='total_bill', y = "tip", data = tips, hue = "size", palette="deep", style = "time", size = "size")
+# 3. 섬(island)에 따른 몸무게 분포, violinplot
+plt.figure(figsize=(8, 6))
+sns.violinplot(x="island", y="body_mass_g", data=penguins, palette="pastel")
+plt.show()
 
 
-sns.catplot( x = "day", y = "total_bill", data = tips, )
-sns.pairplot(data = tips, hue = "time")
-sns.regplot(data=tips, x = "total_bill", y = "tips", hue = "time")
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# flights 데이터셋 로드
+flights = sns.load_dataset("flights")
+
+# 1. 연도(year)별 승객 수(passengers)의 평균을, lineplot
+year_avg = flights.groupby("year")["passengers"].mean().reset_index()
+
+plt.figure(figsize=(8, 6))
+sns.lineplot(x="year", y="passengers", data=year_avg, color="blue")
+plt.show()
+
+# 2. 연도와 월별 승객 수, heatmap
+flights_pivot = flights.pivot(index = "month", columns = "year", values = "passengers")
+plt.figure(figsize=(10, 8))
+sns.heatmap(flights_pivot, annot=True, fmt="d", cmap="coolwarm", cbar=True)
+plt.show()
+
+# 3. 특정 연도(1958년)의 월별 승객 수, barplot
+flight1958 = flights[flights["year"] == 1958]
+sns.barplot(data=flight1958, x="month", y="passengers")
+plt.show()
+'''
+
+
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# titanic 데이터셋 로드
+titanic = sns.load_dataset("titanic")
+
+# 1. 탑승 클래스(class)와 생존 여부(survived)의 관계를 catplot으로 시각화
+sns.catplot(data=titanic, x="class", hue="survived", kind="count")
+plt.show()
+
+# 2
+plt.figure(figsize=(10, 6))
+sns.kdeplot(data=titanic, x="age", hue="survived")
+plt.show()
